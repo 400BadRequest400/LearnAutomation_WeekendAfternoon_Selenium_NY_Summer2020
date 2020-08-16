087fd2fcb50c6f735cd82ea067bcafdf3dc93945
mhshahib <mh.shahib@gmail.com>
2020-08-16 17:46:39 -0400

    first commit

---
+++ src/homepage/SearchBox.java
@@ -0,0 +1,25 @@
+package homepage;
+
+
+
+public class SearchBox {
+
+
+//  #Search box functionality test: Pass
+//    Open Chrome browser: Pass
+//    Enter amazon url in address bar and hit enter: Pass
+//    Click on search box: Pass
+//    Enter Hand sanitizer: Pass
+//    Click on search button: pass
+//    Verify Hand sanitizer is displayed properly: Pass
+//    ExpectedResult: Hand sanitizer should display properly
+//    ActualResult: Hand sanitizer display properly
+
+
+
+
+
+
+
+
+}
